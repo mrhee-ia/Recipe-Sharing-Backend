cae8b024c4346c52eaa39e31799aa9e22ea965b3
mrhee-ia <mcrispasague@gmail.com>
2024-08-11 23:29:53 +0800

    views.py and urls.py was readied for api applications.

--- RSBase/urls.py
+++ RSBase/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import UserViewSet, RecipeViewSet, ProcedureViewSet, LikeViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'recipes', RecipeViewSet)
+router.register(r'procedures', ProcedureViewSet)
+router.register(r'likes', LikeViewSet)
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
 ]
--- RSBase/views.py
+++ RSBase/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Recipe, Procedure, Like, Comment
+from .serializers import UserSerializer, RecipeSerializer, ProcedureSerializer, LikeSerializer, CommentSerializer
 
-# Create your views here.
 
-from django.http import HttpResponse
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
 
-def index(request):
-    return HttpResponse("Hello, World!")
+
+class ProcedureViewSet(viewsets.ModelViewSet):
+    queryset = Procedure.objects.all()
+    serializer_class = ProcedureSerializer
+
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
